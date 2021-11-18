1d4fd02d888b1aecca319c3511e389794b86e079
Adam Wąsowicz <adam.wasowicz@student.po.edu.pl>
2021-11-18 18:17:16 +0100

    small fixes

--- src/helpers/likePostHelper.js
+++ src/helpers/likePostHelper.js
@@ -1,35 +1,33 @@
 import axios from "axios";
-import { API_address, likeComment_API_route } from "../Constants";
+import {
+    API_address,
+    likeComment_API_route
+} from "../Constants";
 
 
-export async function LikePostHelper_GET(jwt, postId)
-{
+export async function LikePostHelper_GET(jwt, postId) {
     let error = false;
 
-    console.log(`${API_address}${likeComment_API_route}${postId}`);
-
-        await axios({
-            method: 'GET',
-            url: `${API_address}${likeComment_API_route}` + postId,
-            headers: {
-                Authorization: "Bearer " + jwt
-            },
-        }).then(result => {
-            if (result.status === 200)
-            {
-                console.log(result);
-            }
-            else
-            {
-                error = true;
-            }
-
+    await axios({
+        method: 'GET',
+        url: `${API_address}${likeComment_API_route}` + postId,
+        //url: `https://localhost:44363/api/v1/likedpost/post/id/${postId}`,
+        headers: {
+            Authorization: "Bearer " + jwt
+        },
+    }).then(result => {
+        if (result.status === 200) {
             console.log(result);
-        }).catch(e => {
-            console.log(e);
+        } else {
             error = true;
-        });
-        
-        console.log('LikePostHelper_GET : END')
-      return error; 
+        }
+
+        console.log(result);
+    }).catch(e => {
+        console.log(e);
+        error = true;
+    });
+
+    console.log('LikePostHelper_GET : END')
+    return error;
 }
