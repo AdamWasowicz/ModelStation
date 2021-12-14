9c04679c85403ac68fcb6ed071a7a4d190cebce9
Adam Wąsowicz <adam.wasowicz@student.po.edu.pl>
2021-12-14 20:50:55 +0100

    STYLE: CreatePost

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,73 +1,76 @@
-import React, {useState, useEffect, useContext, useRef, useMemo } from 'react';
-import bemCssModules from 'bem-css-modules'
+import React, { useState, useEffect, useContext, useRef, useMemo } from 'react';
 import { uploadPost } from '../../helpers/PostHelper';
 import StoreProvider, { StoreContext } from '../../store/StoreProvider';
 
 
 //Styles
 import { default as CreatePostStyle } from './CreatePost.module.scss'
-const bemstyle = bemCssModules(CreatePostStyle);
 
 
 //Components
-import PostCategoryContainer from '../PostCategoryContainer';
 import NotLoggedException from '../NotLoggedException';
 
 
 const CreatePost = () => {
+
     //useContext
     const { isLoggedIn } = useContext(StoreContext);
 
 
+    //useState
+    const [title, setTitle] = useState("");
+    const [text, setText] = useState("");
+    const [postCategoryName, setPostCategoryName] = useState("");
+    const [files, setFiles] = useState({});
+
+
+    //Handlers
+    const handleTitleChange = (event) => setTitle(event.target.value);
+    const handleTextChange = (event) => setText(event.target.value);
+    const handleFileChange = (event) => setFiles(event.target.files);
+    const handlePostCategoryNameChange = (event) => setPostCategoryName(event.target.value);
+    const handleUpload = async () => await uploadPost(JSON.parse(window.localStorage.getItem('jwt')), title, text, postCategoryName, files);
+
+
     if (isLoggedIn) {
-        //useState
-        const [title, setTitle] = useState("");
-        const [text, setText] = useState("");
-        const [postCategoryName, setPostCategoryName] = useState("");
-        const [files, setFiles] = useState({});
-
-
-        //Handlers
-        const handleTitleChange = (event) => setTitle(event.target.value);
-        const handleTextChange = (event) => setText(event.target.value);
-        const handleFileChange = (event) => setFiles(event.target.files);
-        const handlePostCategoryNameChange = (event) => setPostCategoryName(event.target.value);
-        const handleUpload = async () => await uploadPost(JSON.parse(window.localStorage.getItem('jwt')), title, text, postCategoryName, files);
-
 
         return (
             <React.Fragment>
-                <div className={bemstyle('')}>
-                    <h1>Tworzenie nowego postu</h1>
-                    <div>
-                        <div>
-                            <label htmlFor="title">Tytuł: </label>
-                            <input type="text" id="title" value={title} onChange={handleTitleChange} />
-                        </div>
+                <div className='CreatePost'>
+                <div className='RightPart'>
+                        <div className='Title'>Nowy post</div>
 
-                        <div>
-                            <label htmlFor="text">Zawartość: </label>
-                            <input type="text" id="text" value={text} onChange={handleTextChange} />
-                        </div>
+                        <div className='PostData'>
+                            <div className='InputField'>
+                                <div className='InputFieldLabel'>Tytuł: </div>
+                                <input type="text" id="title" value={title} onChange={handleTitleChange} />
+                            </div>
 
-                        <div>
-                            <label htmlFor="text">Kategoria: </label>
-                            <input type="text" disabled={true} id="text" value={postCategoryName} onChange={handlePostCategoryNameChange} />
-                        </div>
+                            <div className='InputField'>
+                                <div className='InputFieldLabel'>Kategoria: </div>
+                                <input type="text" id="text" value={postCategoryName} onChange={handlePostCategoryNameChange} />
+                            </div>
 
-                        <div>
-                            <label htmlFor="file">Zdjęcia: </label>
-                            <input type="file" id="file" accept=".png, .jpg, .jpeg" onChange={handleFileChange} />
-                        </div>
+                            <div className='InputFieldBig'>
+                                <div className='InputFieldLabelBig'>Zawartość: </div>
+                                <textarea className='TextAreaInput' type="text" id="text" value={text} onChange={handleTextChange} />
+                            </div>
 
-                        <button onClick={handleUpload}>
-                            Upload
-                        </button>
+                            <div className='InputField'>
+                                <div className='InputFieldLabel'>Zdjęcie: </div>
+                                <input className='FileInput' type="file" id="file" accept=".png, .jpg, .jpeg" onChange={handleFileChange} />
+                            </div>
+
+                            <button className='Button' onClick={handleUpload}>
+                                Stwórz
+                            </button>
+                        </div>
                     </div>
 
-                    <div>
-                        <PostCategoryContainer setCategoryName={setPostCategoryName} />
-                    </div>
+                    <img
+                        className='LeftPart'>
+
+                    </img>                
                 </div>
             </React.Fragment>
         );
